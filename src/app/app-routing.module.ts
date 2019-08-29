ff8f2ef40d53d38c99de63bbd9db960baf885e2c
pritamkamble <7276279026.pk@gmail.com>
2019-08-29 15:54:43 +0530

    Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DraggableComponent } from './draggable/draggable.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'draggable', pathMatch: 'full' },
+  {
+    path: 'draggable',
+    component: DraggableComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
